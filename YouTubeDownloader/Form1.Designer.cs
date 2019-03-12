a22af7ec32186ce3045d87cc34cb2b713a5bea97
Klemen Košir <klemen.kosir@kream.io>
2019-03-12 07:04:23 +0100

    Update project files

--- YouTubeDownloader/Form1.Designer.cs
+++ YouTubeDownloader/Form1.Designer.cs
@@ -23,12 +23,111 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbURL = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.lblURL = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.pbDownload = new System.Windows.Forms.ToolStripProgressBar();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbURL
+            // 
+            this.tbURL.Location = new System.Drawing.Point(51, 13);
+            this.tbURL.Name = "tbURL";
+            this.tbURL.Size = new System.Drawing.Size(261, 20);
+            this.tbURL.TabIndex = 0;
+            this.tbURL.Click += new System.EventHandler(this.tbURL_Click);
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(237, 39);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 1;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // lblURL
+            // 
+            this.lblURL.AutoSize = true;
+            this.lblURL.Location = new System.Drawing.Point(12, 16);
+            this.lblURL.Name = "lblURL";
+            this.lblURL.Size = new System.Drawing.Size(32, 13);
+            this.lblURL.TabIndex = 2;
+            this.lblURL.Text = "URL:";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus,
+            this.pbDownload});
+            this.statusStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.statusStrip1.Location = new System.Drawing.Point(0, 71);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(324, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(42, 17);
+            this.lblStatus.Text = "Ready.";
+            // 
+            // pbDownload
+            // 
+            this.pbDownload.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.pbDownload.Name = "pbDownload";
+            this.pbDownload.Size = new System.Drawing.Size(100, 16);
+            this.pbDownload.Visible = false;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(15, 39);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(324, 93);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.lblURL);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.tbURL);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "YouTube Downloader";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbURL;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label lblURL;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.ToolStripProgressBar pbDownload;
+        private System.Windows.Forms.Button btnUpdate;
     }
 }
 
--- YouTubeDownloader/Form1.cs
+++ YouTubeDownloader/Form1.cs
@@ -1,17 +1,112 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 using System.Windows.Forms;
 
 namespace YouTubeDownloader {
     public partial class Form1 : Form {
+        // TODO: Download ffmpeg.
+        private String[] dependencies = {
+            "https://yt-dl.org/downloads/latest/youtube-dl.exe",
+        };
+
         public Form1() {
             InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e) {
+            WebClient client = new WebClient();
+
+            foreach (String url in dependencies) {
+                String filename = url.Split('/').Last();
+
+                if (File.Exists(filename) == true) {
+                    continue;
+                }
+
+                // TODO: Progress bar and completed.
+                client.DownloadFileAsync(new Uri(url), filename);
+                pbDownload.Visible = true;
+            }
+        }
+
+        private void DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e) {
+            pbDownload.Value = e.ProgressPercentage;
+        }
+
+        private void DownloadFileCompleted(object sender, EventArgs e) {
+            pbDownload.Visible = false;
+            pbDownload.Value = 0;
+            lblStatus.Text = "Ready.";
+        }
+
+        private void tbURL_Click(object sender, EventArgs e) {
+            (sender as TextBox).SelectAll();
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e) {
+            if (tbURL.Text == "") {
+                return;
+            }
+
+            lblStatus.Text = "Downloading...";
+
+            String destination = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            // TODO: Create YouTube folder.
+
+            try {
+                Process process = new Process();
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.FileName = "youtube-dl.exe";
+                process.StartInfo.Arguments = "-f bestaudio -x --audio-format mp3 " + tbURL.Text + " -o " + destination + "\\YouTube\\%(title)s.%(ext)s";
+                process.StartInfo.CreateNoWindow = true;
+
+                process.EnableRaisingEvents = true;
+                process.Exited += (_sender, _e) => {
+                    if (process.ExitCode == 0) {
+                        lblStatus.Text = "Ready.";
+                        return;
+                    }
+
+                    lblStatus.Text = "Error!";
+                };
+
+                process.Start();
+                process.WaitForExit();
+            } catch (Exception ex) {
+                lblStatus.Text = "Error!";
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e) {
+            lblStatus.Text = "Updating...";
+
+            try {
+                Process process = new Process();
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.FileName = "youtube-dl.exe";
+                process.StartInfo.Arguments = "-U";
+                process.StartInfo.CreateNoWindow = true;
+
+                process.EnableRaisingEvents = true;
+                process.Exited += (_sender, _e) => {
+                    if (process.ExitCode == 0) {
+                        lblStatus.Text = "Ready.";
+                        return;
+                    }
+
+                    lblStatus.Text = "Error!";
+                };
+
+                process.Start();
+                process.WaitForExit();
+            } catch (Exception ex) {
+                lblStatus.Text = "Error!";
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
